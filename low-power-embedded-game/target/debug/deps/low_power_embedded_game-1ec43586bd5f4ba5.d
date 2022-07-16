cc3d408ae8a0f9355cf6a0edd1bcb2f94f5a8f2d
Timothy Cumberland <tdkcumberland@gmail.com>
2022-07-16 13:40:19 -0400

    Delete low-power-embedded-game/target directory

--- low-power-embedded-game/target/debug/deps/low_power_embedded_game-1ec43586bd5f4ba5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Timothy Cumberland\Exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-1ec43586bd5f4ba5.exe: src\lib.rs
-
-C:\Users\Timothy Cumberland\Exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-1ec43586bd5f4ba5.d: src\lib.rs
-
-src\lib.rs:
--- low-power-embedded-game/target/debug/deps/low_power_embedded_game-8d2e1d5aba6c27e5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Timothy Cumberland\Exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-8d2e1d5aba6c27e5.exe: tests\low-power-embedded-game.rs
-
-C:\Users\Timothy Cumberland\Exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-8d2e1d5aba6c27e5.d: tests\low-power-embedded-game.rs
-
-tests\low-power-embedded-game.rs:
