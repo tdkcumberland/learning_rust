c03793605548d78a0b96436d3e98789e9ba83915
Timothy Cumberland <tdkcumberland@gmail.com>
2022-09-10 12:03:43 -0400

    bob

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,27 @@
+use regex::Regex;
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let cap_re = Regex::new(r"[^A-Z]").unwrap();
+    let question = message.trim().ends_with('?');
+    let nothing = message.trim().is_empty() || message.trim().replace('\r', "").is_empty();
+    let all_cap = message.to_uppercase() == message && !cap_re.replace_all(message, "").is_empty();
+
+    if nothing {
+        return "Fine. Be that way!"
+    }
+
+    if question && all_cap {
+        return "Calm down, I know what I'm doing!"
+    }
+
+    if question {
+        return "Sure."
+    }
+
+    if all_cap {
+        return "Whoa, chill out!"
+    }
+
+    return "Whatever."
+
 }
